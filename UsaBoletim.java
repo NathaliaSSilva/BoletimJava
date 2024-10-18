a67880ae030843d7cf329ca75ea3594f8a82da72
Nathalia Santos <147882627+NathaliaSSilva@users.noreply.github.com>
2024-10-17 22:09:51 -0300

    Update UsaBoletim.java

--- UsaBoletim.java
+++ UsaBoletim.java
@@ -8,15 +8,6 @@ public class UsaBoletim {
         System.out.println(b1.VerificaConceito()); 
         
       
-        for(int i =0; i<3;i++ ){
-            for(int j = 0; j<3;j++){
-                if (i==j){
-                   break; 
-                }
-                 System.out.print(i* j +" ");
-            }
-
-           
-        }
+     
     }
 }
